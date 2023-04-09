bf2feaf6791f7ee537e154c73d6adff5bf979cde
Pavel Zaytsev <pavel@iberisoft.com>
2023-04-09 13:02:23 +0200

    Add Logger link

---
+++ HomeExplorer/Models/SerilogSinkSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace HomeExplorer.Models
+{
+    class SerilogSinkSettings
+    {
+        public string Name { get; set; }
+
+        public Dictionary<string, string> Args { get; set; } = new();
+    }
+}
